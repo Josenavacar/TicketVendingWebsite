fe27caad9b70afdfb5720f79ffb0ce1cba2e7f40
I437493 <437493@student.fontys.nl>
2020-10-07 16:26:45 +0200

    Clear code and CI test

--- src/App.js
+++ src/App.js
@@ -4,35 +4,12 @@ import Fans from './components/fans/Fans';
 import './App.css';
 
 class App extends Component {
-  /*
-  state = {
-    isLoading: true,
-    fans: []
-  }
-
-  async componentDidMount() {
-    const response = await fetch('/api/v1/fan');
-    const body = await response.json();
-    this.setState({ fans: body, isLoading: false });
-  }
-  */
-  
-
   render() {
-    
-    /*
-    const {fans, isLoading} = this.state;
-
-    if(isLoading) {
-      return <p>Loading...</p>;
-    }
-    */
-
     return (
       <nav className="App">
         <Navbar />
         <div className="container">
-          <Fans />
+         <Fans />
         </div>
       </nav>
     );
