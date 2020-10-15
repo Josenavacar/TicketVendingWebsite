b955473ba9c847b026371bf91d098f8d16f5912f
I437493 <437493@student.fontys.nl>
2020-10-15 10:14:19 +0200

    changed pom dependency and created a test

--- src/api/src/test/java/com/website/EmailValidatorTest.java
+++ src/api/src/test/java/com/website/EmailValidatorTest.java
@@ -1,19 +1,27 @@
 package com.website;
 
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
 public class EmailValidatorTest {
 
     private final EmailValidator underTest = new EmailValidator();
 
+    @Test
     public void itShouldValidateCorrectEmail() {
         assertThat(underTest.test("hello@gmail.com")).isTrue();
     }
 
+    @Test
     public void itShouldValidateAnIncorrectEmail() {
         assertThat(underTest.test("hellogmail.com")).isFalse();
     }
 
+    @Test
     public void itShouldValidateAnIncorrectEmailWithoutDotAtTheEnd() {
         assertThat(underTest.test("hello@gmail")).isFalse();
     }
