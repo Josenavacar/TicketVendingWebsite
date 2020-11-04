ada4e67b3f927140149bcb69fcbab84e9d5619bd
I437493 <437493@student.fontys.nl>
2020-11-04 04:57:36 +0100

    Changed Front End
    Now there are 2 different pages!

--- BackEnd/src/main/java/com/website/controller/FanController.java
+++ BackEnd/src/main/java/com/website/controller/FanController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 @RequestMapping(path="/api/v1/fan")
 public class FanController {
--- BackEnd/src/main/java/com/website/controller/MatchController.java
+++ BackEnd/src/main/java/com/website/controller/MatchController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 @RequestMapping(path="/api/v1/match")
 public class MatchController {
