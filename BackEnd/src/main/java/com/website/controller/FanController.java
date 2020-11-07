3a530b56fb5b9958304cfc630555920e4f3f9cf6
I437493 <437493@student.fontys.nl>
2020-11-07 15:35:08 +0100

    Added all CRUD functionalities
    They were not implemented due to my change of plans halfway, I forgot to re implement them.

--- BackEnd/src/main/java/com/website/controller/FanController.java
+++ BackEnd/src/main/java/com/website/controller/FanController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @CrossOrigin(origins = "http://localhost:3000")
 @Controller
 @RequestMapping(path="/api/v1/fan")
@@ -23,6 +25,27 @@ public class FanController {
     @GetMapping
     public @ResponseBody Iterable<Fan> getAllFans() {
         return fanRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public @ResponseBody Fan getFanById(@PathVariable int id) {
+        return fanRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("{id}")
+    public @ResponseBody String updateFan(@PathVariable int id, @RequestBody Fan fan) {
+        Fan existingFan = fanRepository.findById(id).orElse(null);
+        existingFan.setUsername(fan.getUsername());
+        existingFan.setPassword(fan.getPassword());
+        existingFan.setEmail(fan.getEmail());
+        fanRepository.save(existingFan);
+        return "Fan by id of " + id + " updated";
+    }
+
+    @DeleteMapping("{id}")
+    public @ResponseBody String deleteFan(@PathVariable int id) {
+        fanRepository.deleteById(id);
+        return "Fan by id " + id + " deleted";
     }
 
 }
--- BackEnd/src/main/java/com/website/controller/MatchController.java
+++ BackEnd/src/main/java/com/website/controller/MatchController.java
@@ -1,5 +1,6 @@
 package com.website.controller;
 
+import com.website.model.Fan;
 import com.website.model.FootballMatch;
 import com.website.repository.MatchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,27 @@ public class MatchController {
     @GetMapping
     public @ResponseBody Iterable<FootballMatch> getAllMatches() {
         return matchRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public @ResponseBody
+    FootballMatch getMatchById(@PathVariable int id) {
+        return matchRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("{id}")
+    public @ResponseBody String updateMatch(@PathVariable int id, @RequestBody FootballMatch match) {
+        FootballMatch existingMatch = matchRepository.findById(id).orElse(null);
+        existingMatch.setTeam1(match.getTeam1());
+        existingMatch.setTeam2(match.getTeam2());
+        existingMatch.setDate(match.getDate());
+        matchRepository.save(existingMatch);
+        return "Match by id of " + id + " updated";
+    }
+
+    @DeleteMapping("{id}")
+    public @ResponseBody String deleteMatch(@PathVariable int id) {
+        matchRepository.deleteById(id);
+        return "Match by id " + id + " deleted";
     }
 }
