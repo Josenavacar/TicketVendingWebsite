dfbc74f48548bb46529650b81aede642bb58bca7
I437493 <437493@student.fontys.nl>
2020-12-20 22:35:43 +0100

    fixed integration test

--- FrontEnd/cypress/integration/home_spec.js
+++ FrontEnd/cypress/integration/home_spec.js
@@ -3,8 +3,6 @@ describe('Home page test', function () {
         cy.visit('http://localhost:3000/home')
 
         cy.contains('Home')
-        cy.contains('Login')
-        cy.contains('Sign Up')
         cy.url()
             .should('includes', '/home')
     })
